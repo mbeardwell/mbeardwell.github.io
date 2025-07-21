f6639cb53fc602df0a15a06ae9670d30e2175353
Matthew Beardwell <mbeardwell@users.noreply.github.com>
2025-07-21 21:05:29 +0100

    "autosave"

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -43,7 +43,7 @@ export default function Projects() {
         <Project
           title="UK Person-centric OSINT Toolkit"
           description="Curation of UK-only, legal person-focused OSINT resources for investigative use. 
-            Featured on OSINTTech (Substack); 100+ LinkedIn/Substack clicks in 1 week"
+            Featured on OSINTTech (Substack); 100+ LinkedIn/Substack clicks in 1 week."
           link="https://github.com/mbeardwell/uk-personal-osint-toolkit"
         />
 
