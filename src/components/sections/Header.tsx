477618ef8e32005a3a3f72d88e1f7506bf2ecc68
Matthew Beardwell <mbeardwell@users.noreply.github.com>
2025-08-14 21:30:10 +0100

    "autosave"

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,39 +1,60 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 import IconPersonal from "@icons/personal-logo.svg?react";
 import IconHamburger from "@icons/hamburger.svg?react";
 import Icon from "@components/Icon";
 
 export default function Header(): JSX.Element {
-    return (
-        <header
-            id="navbar"
-            className="sticky top-0 z-50 w-full bg-accent p-2 flex justify-center"
+  const [burgerMenuOpen, burgerMenuSetOpen] = useState(false);
+
+  return (
+    <>
+    <header
+      id="navbar"
+      className="sticky top-0 z-50 w-full bg-accent p-2 flex justify-center"
+    >
+      <div className="w-full max-w-6xl flex items-center relative">
+        {/* Logo */}
+        <a href="/#home" className="lg:px-4 shrink-0">
+          <Icon
+            Svg={IconPersonal}
+            paintClassName="text-content hover:text-surface !w-8 !h-8"
+          />
+        </a>
+
+        {/* Navigation */}
+        <nav
+          className="ml-auto hidden md:flex gap-6 text-xl text-content px-4"
+          aria-label="Main navigation"
         >
-            <div className="w-full max-w-6xl flex items-center relative">
-                {/* Logo */}
-                <a href="/#home" className="lg:px-4 shrink-0">
-                    <Icon Svg={IconPersonal} paintClassName="text-content hover:text-surface !w-8 !h-8" />
-                </a>
+          <a href="/#home" className="hover:text-surface">
+            Home
+          </a>
+          <a href="/#about" className="hover:text-surface">
+            About
+          </a>
+          <a href="/#projects" className="hover:text-surface">
+            Projects
+          </a>
+          <a href="/#contact" className="hover:text-surface">
+            Contact
+          </a>
+        </nav>
 
-                {/* Navigation */}
-                <nav
-                    className="ml-auto hidden md:flex gap-6 text-xl text-content px-4"
-                    aria-label="Main navigation"
-                >
-                    <a href="/#home" className="hover:text-surface">Home</a>
-                    <a href="/#about" className="hover:text-surface">About</a>
-                    <a href="/#projects" className="hover:text-surface">Projects</a>
-                    <a href="/#contact" className="hover:text-surface">Contact</a>
-                </nav>
+        {/* Hamburger (Mobile) */}
+        <button
+          className="absolute right-0"
+          aria-label="Open menu"
+          onClick={() => burgerMenuSetOpen((o) => !o)}
+        >
+          <Icon
+            Svg={IconHamburger}
+            paintClassName="text-content hover:text-surface !w-8 !h-8"
+          />
+        </button>
 
-                {/* Hamburger (Mobile) */}
-                {/*<button
-                    className="absolute right-0 md:hidden"
-                    aria-label="Open menu"
-                >
-                    <Icon Svg={IconHamburger} paintClass="text-content hover:text-surface !w-8 !h-8" />
-                </button>*/}
-            </div>
-        </header>
-    );
+      </div>
+    </header>
+    {burgerMenuOpen && <div className="z-1000 bg-red-400 h-100 w-100">hello</div>}
+    </>
+  );
 }
