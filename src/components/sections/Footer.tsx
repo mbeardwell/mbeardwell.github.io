88368ee5e009cf6bbc2703183e503ac21f4dd4e4
Matthew Beardwell <mbeardwell@users.noreply.github.com>
2026-04-18 18:15:33 +0100

    Updates

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -3,8 +3,8 @@ import { JSX } from "react";
 export default function Footer(): JSX.Element {
     return (
         <footer className="z-10 bg-surface w-full flex flex-row justify-between gap-4 py-6 px-8">
-            <p className="text-md">Matthew Beardwell &copy; {(new Date()).getFullYear()}</p>
-            <p className="text-md">Developed in React/Tailwind</p>
+            <p className="text-md text-left">Matthew Beardwell &copy; {(new Date()).getFullYear()}</p>
+            <p className="text-md text-right">Developed in React/Tailwind</p>
         </footer>
     );
 }
